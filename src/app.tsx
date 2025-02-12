52139b7759d1db9df26b40035339d63f21fdf35d
Maximiliano Salvatti <msalvatti@gmail.com>
2025-02-12 13:37:39 -0300

    Fix: Telegram scripts inserted only when accessed from Telegram app

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { useRoutes } from 'react-router-dom'
 
+import useWindow from './hooks/useWindow'
 import './languages/i18n'
 import routes from './routes'
 
 const App: React.FC = () => {
   const content = useRoutes(routes)
+  const { getIsTelegramApp } = useWindow()
+  const { isTelegram } = getIsTelegramApp()
+
+  useEffect(() => {
+    if (isTelegram) {
+      const script = document.createElement('script')
+      script.src = 'https://telegram.org/js/telegram-web-app.js'
+      script.async = true
+      document.body.appendChild(script)
+
+      return () => {
+        document.body.removeChild(script)
+      }
+    }
+  }, [isTelegram])
 
   return (
     <HelmetProvider>
